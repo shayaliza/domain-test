7dd833f986852d5dae06087b70855c034a6acbf6
shayaliza <magicalmind01@gmail.comgit statuss>
2024-01-01 18:15:52 +0530

    asd

--- api/dynamicPath/[...path].js
+++ api/dynamicPath/[...path].js
@@ -5,15 +5,9 @@ module.exports = (req, res) => {
 
   let greeting = "there";
 
-  if (Array.isArray(path)) {
-    // Check if the path contains multiple segments
-    if (path.length > 1) {
-      // Use the last element of the array as the greeting
-      greeting = path[path.length - 1];
-    } else if (path.length === 1) {
-      // Use the single element as the greeting
-      greeting = path[0];
-    }
+  if (path) {
+    // If path is a string, set greeting to path
+    greeting = Array.isArray(path) ? path.join("/") : path;
   }
 
   res.status(200).send(`<!DOCTYPE html>
