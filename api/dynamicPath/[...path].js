9f48ac0f3a9905dcf81422d0de9e671593c6f332
shayaliza <magicalmind01@gmail.comgit statuss>
2024-01-01 17:46:12 +0530

    aaaasd

---
+++ api/dynamicPath/[...path].js
@@ -0,0 +1,16 @@
+// api/dynamicPath/[...path].js
+module.exports = (req, res) => {
+  const { path } = req.query;
+  res.setHeader("Content-Type", "text/html");
+  res.status(200).send(`<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Greetings</title>
+      </head>
+      <body>
+        <h1>Hi ${path || "there"}!</h1>
+      </body>
+    </html>`);
+};
