3f32956376f540560499d84d1afb3ff936172b89
shayaliza <magicalmind01@gmail.comgit statuss>
2024-01-01 18:32:51 +0530

    as

--- api/dynamicPath/[...path].js
+++ api/dynamicPath/[...path].js
@@ -1,19 +1,13 @@
 // api/dynamicPath/[...path].js
 module.exports = (req, res) => {
-  const { path } = req.query;
+  const { name } = req.query;
   const fullPath = req.url; // Get the full path URL
   res.setHeader("Content-Type", "text/html");
 
   let greeting = "there";
 
-  if (path) {
-    // If path is an array, use the last element as the greeting
-    if (Array.isArray(path)) {
-      greeting = path[path.length - 1];
-    } else {
-      // If path is a string, use it as the greeting
-      greeting = path;
-    }
+  if (name) {
+    greeting = name;
   }
 
   res.status(200).send(`<!DOCTYPE html>
