21bcff3dda0f34eee28b70f1ab14ea82dd20e7f7
shayaliza <magicalmind01@gmail.comgit statuss>
2024-01-01 17:14:19 +0530

    re

---
+++ script.js
@@ -0,0 +1,18 @@
+document
+  .getElementById("generateLinkBtn")
+  .addEventListener("click", function () {
+    var name = prompt("Enter your name:");
+    if (name) {
+      var link = generateLink(name);
+      document.getElementById("linkDisplay").innerText =
+        "Generated Link: " + link;
+
+      // Redirect to the generated link
+      window.location.href = link;
+    }
+  });
+
+function generateLink(name) {
+  // You can customize the link format as needed
+  return "https://" + name + ".aliza.vercel.app/";
+}
